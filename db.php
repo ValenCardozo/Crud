acc619848623b64bda2a0eba5b2afe2844b2e5e3
ValenCardozo <valencardozo7@gmail.com>
2020-12-17 20:01:58 -0300

    First Commit

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'CRUD'
+);
+//Testeo de conexion
+// if (isset($conn)) {
+//    echo 'DB is connected';
+//}
+
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php include("db.php") ?>
+<?php include("includes/header.php") ?>
+
+<div class="container p-4">
+    <div class="row">
+        <div class="col-md-4">
+
+            <div class="card card-boddy">
+                <form action="">
+                    <div class="form-group">
+                        <input type="text" name="title" class="form-control"
+                        placeholder="Task Title" autofocus>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+
+        <div class="col-md-8">
+
+        </div>
+        
+    </div>
+
+</div>
+
+<?php include("includes/footer.php") ?>
