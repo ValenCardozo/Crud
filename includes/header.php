b2e90d628414a9af8a9a9b5da33ec29ba650bd0e
ValenCardozo <valencardozo7@gmail.com>
2020-12-21 19:15:03 -0300

    Test

--- includes/header.php
+++ includes/header.php
@@ -1,20 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP MYSQL CRUD</title>
     <!-- BOOTSTRAP 4 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <!-- FONT AWESOME 5 -->
     <script src="https://kit.fontawesome.com/add5ca158a.js" crossorigin="anonymous"></script>
 
+    <style media="screen">
+
+        @media (min-width: 992px) {
+            body {
+                background: white;
+                height: screen;
+                width: screen;
+            }
+        }
+    </style>
+
 </head>
 <body>
-
-
-<nav class="navabar navbar-dark bg-dark">
-    <div class="container">
-        <a href="index.php" class="navbar-brand"> CRUD (Create + Read + Update + Delete) </a>
-    </div>
-</nav>
+    
+    <nav class="navabar navbar-dark bg-dark">
+        
+        <div class="">
+            
+            <a href="index.php" class="navbar-brand"> CRUD (Create + Read + Update + Delete) </a>
+        
+        </div>
+    </nav>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,77 +1,105 @@
 <?php include("db.php") ?>
 <?php include("includes/header.php") ?>
+<div class="flex ">
 
-<div class="flex container p-4">
+    <div class="flex p-4">
+           
+        <div class="row justify-content-center align-items-center">
+            
+            <div class="col-md-4">
 
-    <div class="row">
+                        <?php if(isset($_SESSION['message'])) { ?>
+                            <div class="alert alert-<?= $_SESSION['message_type'] ?> alert-dismissible fade show" role="alert">
+                            <?= $_SESSION['message'] ?>
+                            
+                            <button 
+                            type="button" 
+                            class="btn-close" 
+                            data-bs-dismiss="alert" 
+                            aria-label="Close">
+                            </button>
+                            
+                            </div>
+                        
+                        <?php session_unset(); } ?>
 
-        <div class="col-md-4">
+                        <div class="card card-boddy p-4 ">
+                            <form action="save_task.php" method="POST">
+                                
+                                <div class="form-group my-2">
+                                    <input 
+                                    type="text" 
+                                    name="title" 
+                                    class="form-control"
+                                    placeholder="Task Title" 
+                                    autofocus>
+                                </div>
+                                
+                                <div class="form-group my-2">
+                                    <textarea 
+                                    name="description" 
+                                    rows="2" 
+                                    class="form-control"
+                                    placeholder="Task Description"
+                                    >
+                                    </textarea>
+                                </div>
+                                
+                                <input 
+                                type="submit" 
+                                class="btn btn-success btn-block" 
+                                name="save_task"
+                                value="Save Task">
+                            
+                            </form>
+                        </div>
 
-
-            <?php if(isset($_SESSION['message'])) { ?>
-                <div class="alert alert-<?= $_SESSION['message_type'] ?> alert-dismissible fade show" role="alert">
-                 <?= $_SESSION['message'] ?>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            <?php session_unset(); } ?>
-
-
-            <div class="card card-boddy p-4">
-                <form action="save_task.php" method="POST">
-                    <div class="form-group my-2">
-                        <input type="text" name="title" class="form-control"
-                        placeholder="Task Title" autofocus>
-                    </div>
-                    <div class="form-group my-2">
-                        <textarea name="description" rows="2" class="form-control"
-                        placeholder="Task Description"></textarea>
-                    </div>
-                    <input type="submit" class="btn btn-success btn-block" name="save_task"
-                    value="Save Task">
-                </form>
             </div>
+            
+            <div class="col-md-8">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>Title</th>
+                                    <th>Description</th>
+                                    <th>Created At</th>
+                                    <th>Status</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <?php 
+                            $query = "SELECT * FROM task";
+                            $resul_tasks =mysqli_query($conn, $query);
+                            
+                            while($row = mysqli_fetch_array($resul_tasks)) { ?>
+                                    <tr>
+                                        <td> <?php echo $row['Title']; ?> </td>
+                                        <td> <?php echo $row['Description']; ?> </td>
+                                        <td> <?php echo $row['Created_AD']; ?> </td>
+                                        <td>
+                                            In progress
+                                        </td>
+                                        <td> 
+                                            <a href="edit_task.php?id=<?php echo $row['ID']?>" class="btn btn-secondary">
+                                                <i class="fas fa-marker"></i>
+                                            </a>
+                                            
+                                            <a href="delete_task.php?id=<?php echo $row['ID']?>" class="btn btn-danger">
+                                                <i class="far fa-trash-alt"></i>
+                                            </a>
+                                        
+                                        </td>
+                                    </tr>
 
+
+
+                            <?php } ?> 
+                            </tbody>
+                        </table>
+            </div>    
+            
         </div>
-
-        <div class="col-md-8">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Description</th>
-                        <th>Created At</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                   <?php 
-                   $query = "SELECT * FROM task";
-                   $resul_tasks =mysqli_query($conn, $query);
-                   
-                   while($row = mysqli_fetch_array($resul_tasks)) { ?>
-                        <tr>
-                            <td> <?php echo $row['Title']; ?> </td>
-                            <td> <?php echo $row['Description']; ?> </td>
-                            <td> <?php echo $row['Created_AD']; ?> </td>
-                            <td> 
-                                <a href="edit_task.php?id=<?php echo $row['ID']?>" class="btn btn-secondary">
-                                    <i class="fas fa-marker"></i>
-                                </a>
-                                <a href="delete_task.php?id=<?php echo $row['ID']?>" class="btn btn-danger">
-                                    <i class="far fa-trash-alt"></i>
-                                </a>
-                            </td>
-                        </tr>
-
-
-
-                   <?php } ?> 
-                </tbody>
-            </table>
-        </div>
-        
     </div>
-
-</div>
-
+</div>    
 <?php include("includes/footer.php") ?>
